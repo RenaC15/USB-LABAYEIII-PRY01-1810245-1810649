b1370f2b118dab6b9a9ab0cf95213dc4ab2f4065
RenaC15 <163904941+RenaC15@users.noreply.github.com>
2025-02-19 19:27:17 -0400

    Update GrafoR-1810245-1810649.kt

--- GrafoR-1810245-1810649.kt
+++ GrafoR-1810245-1810649.kt
@@ -6,13 +6,10 @@ import libGrafoR.*
 fun main(args: Array<String>) {
 // Halla la ruta menos costosa del autobús dados los datos
 // en un archivo externo que se espera recibir
-
     val archivo = File("grafos/rutaOptUSB.txt")
     val lineas = archivo.readLines()
     // La primera línea es el número de vértices
     val vertices = lineas[0].toInt()
-
-
     var grafond = GrafoNoDirigidoCosto (vertices+1)
     // la segunda línea es el número de aristas
     //val aristas = lineas[1].toInt()
@@ -33,20 +30,12 @@ fun main(args: Array<String>) {
         count = count + 1
     }
 
-
-
     for (i in 2 + vertices until lineas.size) {
-
         var (bus, inicio, fin, costoStr) = lineas[i].split(" ")
-
         var u = paradas[inicio] ?: throw IllegalArgumentException("Parada no encontrada: $inicio")
-
         // excepcón que indica que el punto de partida debe ser ser la usb
         if ( i == 2 && u != 0){ throw Exception("El punto de partida debe ser la USB") }
-
-
         var v = paradas[fin] ?: throw IllegalArgumentException("Parada no encontrada: $fin")
-
         try{
             var costo = costoStr.toDouble()
             grafond.agregarAristaCosto( AristaCosto(u, v, costo))
@@ -54,21 +43,14 @@ fun main(args: Array<String>) {
             throw Exception("El costo no tiene el formato adecuado")
         }
     }
-
-
+    
     var lados = grafond.iterator()
     var lista_aristas = mutableListOf<AristaCosto>()
-
     for ( arista in lados ){
         lista_aristas.add(arista)
     }
-
-
     // TENER CUIDADO CON EL NUMERO DE VERTICES
- 
-
     var mst = kruskal(lista_aristas, grafond.obtenerNumeroDeVertices())
-
     for (edge in mst) {
         var parada_i = String()
         var parada_f = String()
@@ -83,6 +65,5 @@ fun main(args: Array<String>) {
         } 
         println("$parada_i --(${edge.costo} Lt)-- $parada_f")
     }
-
 }
 
